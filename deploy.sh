1ac03e6274f83fc1db778ef13047b557b4b1fdb5
shihang <study1228.sh@gmail.com>
2014-03-17 19:33:15 +0800

    修改deploy复制servlet-api和sql connector

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 
 cp -r src/main/webapp/* $CATALINA_HOME/webapps/webservlet/
 cp -r  target/classes/  $CATALINA_HOME/webapps/webservlet/WEB-INF/
+cp -r  lib $CATALINA_HOME/webapps/webservlet/WEB-INF/

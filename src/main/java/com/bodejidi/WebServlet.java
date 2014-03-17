e75762a0085b67e1c82325081019c2375a51a552
shihang <study1228.sh@gmail.com>
2014-03-17 17:45:10 +0800

    用dopost方法实现了参数传递

--- src/main/java/com/bodejidi/WebServlet.java
+++ src/main/java/com/bodejidi/WebServlet.java
@@ -7,10 +7,16 @@ import java.io.*;
 public class WebServlet extends HttpServlet
 {
     public void doGet(HttpServletRequest req,HttpServletResponse resp) 
-                      throws IOException,ServletException
+                      throws IOException, ServletException
     {
         resp.getWriter().println("hello webservlet!");
     }		      
-		          
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+    			throws IOException, ServletException
+    {
+    	String firstName = req.getParameter("first_name");
+	String lastName = req.getParameter("last_name");
+	resp.getWriter().println("    " + firstName + "  " + lastName );
+    }
 		      
 }

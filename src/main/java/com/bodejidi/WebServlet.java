46e6460b75961e4a91dce548a4c3afb049f396aa
shihang <study1228.sh@gmail.com>
2014-03-17 19:12:51 +0800

    成功链接数据库

--- src/main/java/com/bodejidi/WebServlet.java
+++ src/main/java/com/bodejidi/WebServlet.java
@@ -3,6 +3,7 @@ package com.bodejidi;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.sql.*;
 
 public class WebServlet extends HttpServlet
 {
@@ -14,9 +15,53 @@ public class WebServlet extends HttpServlet
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
     			throws IOException, ServletException
     {
-    	String firstName = req.getParameter("first_name");
-	String lastName = req.getParameter("last_name");
-	resp.getWriter().println("    " + firstName + "  " + lastName );
+		//resp.setContentType("text/html; charset=UTF-8");
+		Connection conn = null;
+		Statement stmt = null;
+		//resp.getWriter().println("    " + firstName + "  " + lastName );
+		try
+       	{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/test?"
+												+ "user=root" + "&password=");
+			resp.getWriter().println("Connect data base success");
+		}
+		catch (Exception ex)
+       	{
+            
+       	}
+		finally 
+		{
+           
+           	if (stmt != null)
+			{
+              	try
+				{
+                    stmt.close();
+               	}
+				catch (SQLException sqlEx)
+				{
+                  
+              	}
+                stmt = null;
+			}
+
+           	if (conn != null)
+			{
+                try 
+				{
+                   	conn.close();
+              	} 
+				catch (SQLException sqlEx)
+				{
+                   
+               	}
+                conn = null;
+            }
+    
+		}
+			
+		
     }
 		      
 }
